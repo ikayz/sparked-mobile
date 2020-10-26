008cb3262ceebb403bfabebd035ab6adb83d3e05
Dennis Mubamba <dennism501@outlook.com>
2020-10-26 15:49:57 +0200

    Added folder structure

---
+++ src/views/Onboarding.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {SafeAreaView} from 'react-native'
+import OnboardingScreen from './components/OnboardingScreen'
+export default function OnboardingScreen() {
+  return (
+    <SafeAreaView>
+      <OnboardingScreen />
+    </SafeAreaView>
+  )
+}
